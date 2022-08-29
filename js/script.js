ae1d7a7e62e806ec6658f302a9e5603ec6a04f30
BlishchAndrew <blishch.andrew@gmail.com>
2022-08-29 17:46:38 +0300

    Update 2022-08-29T14:46:34.414Z

--- js/script.js
+++ js/script.js
@@ -324,8 +324,8 @@ if (document.querySelector('.slider-event__wrapper')) {
 			sensitive: 1
 		},
 		navigation: {
-			nextEl: '.slider-event-control__arrow_next',
-			prevEl: '.slider-event-control__arrow_prev',
+			nextEl: '.event-control-next',
+			prevEl: '.event-control-prev',
 		},
 		breakpoints: {
 			565: {
@@ -337,6 +337,35 @@ if (document.querySelector('.slider-event__wrapper')) {
 			1400: {
 				slidesPerView: 4,
 			},
+		},
+	});
+}
+if (document.querySelector('.slider-webinars__wrapper')) {
+	new Swiper('.slider-webinars__wrapper', {
+		observer: true,
+		watchOverflow: true,
+		observeParents: true,
+		slidesPerView: 1,
+		parallax: true,
+		spaceBetween: 24,
+		speed: 800,
+		mousewheel: {
+			sensitive: 1
+		},
+		navigation: {
+			nextEl: '.webinar-control-next',
+			prevEl: '.webinar-control-prev',
+		},
+		breakpoints: {
+			// 565: {
+			// 	slidesPerView: 2,
+			// },
+			// 992: {
+			// 	slidesPerView: 3,
+			// },
+			// 1400: {
+			// 	slidesPerView: 4,
+			// },
 		},
 	});
 }
