8e2520e5d17d5e3065719b135fb7416467b4b33d
InfoCenterBy <94043508+SVENmods@users.noreply.github.com>
2022-12-20 15:33:46 +0300

    Udpdate demo

--- js/script.js
+++ js/script.js
@@ -803,3 +803,8 @@ getExchangeRates();
     });
   });
 })(jQuery);
+
+var popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'))
+var popoverList = popoverTriggerList.map(function (popoverTriggerEl) {
+  return new bootstrap.Popover(popoverTriggerEl)
+})
