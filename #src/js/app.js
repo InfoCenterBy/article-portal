545faf4ad8ddeb738b6e791d837365e0a598e294
InfoCenterBy <94043508+SVENmods@users.noreply.github.com>
2022-12-20 15:32:15 +0300

    Made subscribe-new and update script

--- #src/js/app.js
+++ #src/js/app.js
@@ -269,3 +269,8 @@ getExchangeRates();
     });
   });
 })(jQuery);
+
+var popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'))
+var popoverList = popoverTriggerList.map(function (popoverTriggerEl) {
+  return new bootstrap.Popover(popoverTriggerEl)
+})
