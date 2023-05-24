e1e11d292bc4c6c824e2ec1c78c04c7489eed6a9
InfoCenterBy <94043508+SVENmods@users.noreply.github.com>
2023-05-24 09:36:57 +0300

    Moment js fix

--- #src/js/app.js
+++ #src/js/app.js
@@ -299,6 +299,8 @@ $(function () {
       close: 'bi bi-x', // Иконка "Закрыть"
     },
     minDate: minDate,
-    theme: 'bootstrap'
+    theme: 'bootstrap',
+    language: 'ru',
+    locale: 'ru'
   });
 });
