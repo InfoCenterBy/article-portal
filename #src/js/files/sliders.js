74e9c3b78c86244f30fdb8eb36f5fa8bbb75f4c1
dyamorph <dyamorph22@gmail.com>
2024-09-10 15:59:35 +0300

    add slider for infographics

--- #src/js/files/sliders.js
+++ #src/js/files/sliders.js
@@ -1,58 +1,75 @@
-if (document.querySelector('.slider-event__wrapper')) {
-	new Swiper('.slider-event__wrapper', {
-		observer: true,
-		watchOverflow: true,
-		observeParents: true,
-		slidesPerView: 1,
-		parallax: true,
-		spaceBetween: 24,
-		speed: 800,
-		mousewheel: {
-			sensitive: 1
-		},
-		navigation: {
-			nextEl: '.event-control-next',
-			prevEl: '.event-control-prev',
-		},
-		breakpoints: {
-			565: {
-				slidesPerView: 2,
-			},
-			992: {
-				slidesPerView: 3,
-			},
-			1400: {
-				slidesPerView: 4,
-			},
-		},
-	});
+if (document.querySelector(".slider-event__wrapper")) {
+  new Swiper(".slider-event__wrapper", {
+    observer: true,
+    watchOverflow: true,
+    observeParents: true,
+    slidesPerView: 1,
+    parallax: true,
+    spaceBetween: 24,
+    speed: 800,
+    mousewheel: {
+      sensitive: 1,
+    },
+    navigation: {
+      nextEl: ".event-control-next",
+      prevEl: ".event-control-prev",
+    },
+    breakpoints: {
+      565: {
+        slidesPerView: 2,
+      },
+      992: {
+        slidesPerView: 3,
+      },
+      1400: {
+        slidesPerView: 4,
+      },
+    },
+  });
 }
-if (document.querySelector('.slider-webinars__wrapper')) {
-	new Swiper('.slider-webinars__wrapper', {
-		observer: true,
-		watchOverflow: true,
-		observeParents: true,
-		slidesPerView: 1,
-		parallax: true,
-		spaceBetween: 24,
-		speed: 800,
-		mousewheel: {
-			sensitive: 1
-		},
-		navigation: {
-			nextEl: '.webinar-control-next',
-			prevEl: '.webinar-control-prev',
-		},
-		breakpoints: {
-			// 565: {
-			// 	slidesPerView: 2,
-			// },
-			// 992: {
-			// 	slidesPerView: 3,
-			// },
-			// 1400: {
-			// 	slidesPerView: 4,
-			// },
-		},
-	});
+if (document.querySelector(".slider-webinars__wrapper")) {
+  new Swiper(".slider-webinars__wrapper", {
+    observer: true,
+    watchOverflow: true,
+    observeParents: true,
+    slidesPerView: 1,
+    parallax: true,
+    spaceBetween: 24,
+    speed: 800,
+    mousewheel: {
+      sensitive: 1,
+    },
+    navigation: {
+      nextEl: ".webinar-control-next",
+      prevEl: ".webinar-control-prev",
+    },
+    breakpoints: {
+      // 565: {
+      // 	slidesPerView: 2,
+      // },
+      // 992: {
+      // 	slidesPerView: 3,
+      // },
+      // 1400: {
+      // 	slidesPerView: 4,
+      // },
+    },
+  });
+}
+
+if (document.querySelector(".slider__infographics__wrapper")) {
+  new Swiper(".slider__infographics__wrapper", {
+    observer: true,
+    watchOverflow: true,
+    observeParents: true,
+    slidesPerView: 1,
+    parallax: true,
+    spaceBetween: 24,
+    speed: 800,
+
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
 }
